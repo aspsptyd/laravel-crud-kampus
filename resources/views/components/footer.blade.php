559bc65dc9351b4871bfe91dc36a911f5b0204ea
aspsptyd <devadiroot@gmail.com>
2023-04-13 14:44:47 +0700

    Sesuaikan instansi untuk footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,3 @@
 <div class="my-3" style="text-align: center;">
-    Copyright © <?php echo "2022-". date("Y"); ?> Asep Septiadi. All right reserved.
+    Copyright © <?php echo "2022-". date("Y"); ?> Kampus Kita. All right reserved.
 </div>
