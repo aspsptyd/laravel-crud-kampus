4fc328ab04f3310394380bdb644dd86c3bcaa54f
aspsptyd <devadiroot@gmail.com>
2023-04-13 22:14:07 +0700

    Update model fillable jenis kelamin dan foto mahasiswa

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Mahasiswa extends Model
 {
     use HasFactory;
-    protected $fillable = ['nim', 'nama', 'jurusan'];
+    protected $fillable = ['nim', 'foto_mahasiswa', 'nama', 'jenis_kelamin', 'jurusan'];
     protected $table = 'mahasiswa';
     public $timestamps = true;
 }
