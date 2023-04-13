b6c1c3648f5bfcbc3161062bfd7f9e09e264169e
aspsptyd <devadiroot@gmail.com>
2023-04-13 22:15:32 +0700

    Tambah inputan jenis kelamin dan photo untuk mengupdate data mahasiswa jika tidak ada jenis kelamin atau photo

--- resources/views/mahasiswa/edit.blade.php
+++ resources/views/mahasiswa/edit.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     <!-- START FORM -->
-    <form action='{{ url('mahasiswa/'.$data->nim) }}' method='post'>
+    <form enctype="multipart/form-data" action='{{ url('mahasiswa/'.$data->nim) }}' method='post'>
     @csrf
     @method('PUT')
     <div class="my-0 p-5 bg-body rounded shadow-sm">
@@ -32,9 +32,37 @@
             </div>
         </div>
         <div class="mb-3 row">
+            <label for="jk" class="col-sm-2 col-form-label">Jenis Kelamin</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" value="{{ $data->jenis_kelamin }}" name='jk' id="jk" placeholder="Anda belum mengisi Jenis Kelamin">
+            </div>
+        </div>
+        <div class="mb-0 row">
+            <label for="fotomhs" class="col-sm-2 col-form-label">Update Photo</label>
+            <div class="col-sm-10">
+                <input type="file" class="form-control @error('fotomhs') is-invalid @enderror" value="{{ $data->jenis_kelamin }}" id="fotomhs" name="fotomhs"
+                accept="image/*" onchange="document.getElementById('output').src = window.URL.createObjectURL(this.files[0])">
+                @error('fotomhs')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>Error load image</strong>
+                    </span>
+                @enderror
+            </div>
+        </div>
+        <div class="mb-3 row">
+            <label class="col-sm-2 col-form-label"></label>
+            <div class="col-sm-10">
+                @if ($data->foto_mahasiswa != null || $data->foto_mahasiswa != '')
+                    <div class="mt-3"><img style="border-radius: 50%;" src="{{ url('pictures').'/'.$data->foto_mahasiswa }}" id="output" width="200"></div>
+                @else 
+                    <div class="mt-3"><img style="border-radius: 50%;" src="{{ asset('img/default-profile.png') }}" id="output" width="200"></div>
+                @endif
+            </div>
+        </div>
+        <div class="mb-3 row">
             <label for="jurusan" class="col-sm-2 col-form-label"></label>
             <div class="col-sm-10">
-                <a href="{{ url('mahasiswa') }}" class="btn btn-success"><i class="bi bi-arrow-left"></i></a>
+                <a href="{{ url('mahasiswa') }}" class="btn btn-outline-success"><i class="bi bi-arrow-left"></i> Kembali</a>
                 <button type="submit" class="btn btn-primary" name="submit">Simpan Perubahan</button>
             </div>
         </div>
